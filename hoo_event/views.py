4a1f90fe553e7b60f298ed7659a8e66a72ec2b6b
seankatauskas <seankatauskas@outlook.com>
2023-10-07 23:11:35 -0400

    Removed logic

--- hoo_event/views.py
+++ hoo_event/views.py
@@ -5,6 +5,4 @@ from django.contrib.auth.decorators import login_required
 def main(request):
     if request.user.is_staff:
         return redirect('admin:index')
-    elif request.user.email == "durumichael010@gmail.com" or request.user.email == "danielhuynh523@gmail.com" or request.user.email == "seankatauskas1@gmail.com":
-        return redirect('admin:index')
     return render(request, 'index.html')
