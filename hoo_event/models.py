76d51d9ee3527189ef865b18313be7b62f49f4b3
seankatauskas <seankatauskas@outlook.com>
2023-10-18 21:14:36 -0400

    added admin and regular user groups with mock admin_user page

--- hoo_event/models.py
+++ hoo_event/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import Group, Permission
+
+regular_users, created = Group.objects.get_or_create(name ='regular_users')
+admin_users, created = Group.objects.get_or_create(name ='admin_users')
+
+
--- hoo_event/views.py
+++ hoo_event/views.py
@@ -6,6 +6,11 @@
 # *  Version: 4.2
 # *  URL: https://docs.djangoproject.com/en/4.2/topics/auth/default/
 # *
+# *  Title: User group get method filter.exists
+# *  Author: Charlesthk
+# *  Date: October 18th, 2023
+# *  URL: https://stackoverflow.com/questions/4789021/in-django-how-do-i-check-if-a-user-is-in-a-certain-group
+# *
 # ***************************************************************************************/
 
 
@@ -14,6 +19,8 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def main(request):
-    if request.user.is_staff:
+    if request.user.groups.filter(name='admin_users').exists():
+        return render(request, 'admin_event.html')
+    elif request.user.is_staff:
         return redirect('admin:index')
     return render(request, 'index.html')
