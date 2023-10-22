7ce37ca9edc53e571d2ab9c5c5b228889ec11ace
nathan <myr4vt@virginia.edu>
2023-10-22 15:58:28 -0400

    Testing

--- hoo_event/tests.py
+++ hoo_event/tests.py
@@ -46,28 +46,23 @@ class EventModelTest(unittest.TestCase):
         self.assertEqual(str(event), self.event_data['event_title'])
 
 ##################
-    def tearDown(self):
-        # Clean up the database
-        self.user.delete()
+
+class EventViewTest(TestCase):
     def setUp(self):
-        # Create a test user with a unique username
-        self.user = User.objects.create_user(
-            username=f"unique_testuser_{int(time.time())}",  # Append a timestamp
-            password="testpassword",
-        )
-        # Set up the test client
         self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
 
     def test_add_event_view_accessible_by_logged_in_user(self):
-        self.client.login(username=self.user.username, password="testpassword")
+        self.client.login(username='testuser', password='testpassword')
         response = self.client.get(reverse('addNewEvent'))
         self.assertEqual(response.status_code, 200)
-
 
     def test_add_event_view_redirects_to_login_for_anonymous_user(self):
         response = self.client.get(reverse('addNewEvent'))
         self.assertRedirects(response, reverse('login') + f'?next={reverse("addNewEvent")}')
-
 
     def test_event_detail_view(self):
         event = Event.objects.create(
@@ -81,7 +76,3 @@ class EventModelTest(unittest.TestCase):
         response = self.client.get(reverse('event', args=[event.id]))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Test Event")
-
-    def test_recent_events_view(self):
-        response = self.client.get(reverse('recent'))
-        self.assertEqual(response.status_code, 200)
