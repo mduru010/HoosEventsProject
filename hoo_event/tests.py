c7e8309efe53701aa5d455ae7e3f6452c6167b65
nathan <myr4vt@virginia.edu>
2023-10-22 15:22:22 -0400

    Testing

--- hoo_event/tests.py
+++ hoo_event/tests.py
@@ -25,17 +25,7 @@ class EventModelTest(unittest.TestCase):
         event.event_city = "New York"
         event.event_state = "NY"
 
-        # event = Event.objects.create(**event_data)
-        # event_data = {
-        #     'event_title': "Test Event",
-        #     'event_latitude': 40.7128,
-        #     'event_longitude': -74.0060,
-        #     'event_street_address': "123 Main St",
-        #     'event_city': "New York",
-        #     'event_state': "NY",
-        # }
 
-        # Check if the event was created successfully
         self.assertEqual(event.event_title, self.event_data['event_title'])
         self.assertEqual(event.event_latitude, self.event_data['event_latitude'])
         self.assertEqual(event.event_longitude, self.event_data['event_longitude'])
@@ -53,36 +43,34 @@ class EventModelTest(unittest.TestCase):
         event.event_state = "NY"
         # Check if the string representation of the event is as expected
         self.assertEqual(str(event), self.event_data['event_title'])
-    # def test_add_event_view_accessible_by_logged_in_user(self):
-    #     self.client.login(username="testuser", password="testpassword")
-    #     response = self.client.get(reverse('addNewEvent'))
 
-    #     # Check if the response status code is 200 (OK)
-    #     self.assertEqual(response.status_code, 200)
+    def test_add_event_view_accessible_by_logged_in_user(self):
+        user = User.objects.create_user(
+            username="testuser",
+            password="testpassword",
+        )
+        self.client.login(username="testuser", password="testpassword")
+        response = self.client.get(reverse('addNewEvent'))
+        self.assertEqual(response.status_code, 200)
 
+    def test_add_event_view_redirects_to_login_for_anonymous_user(self):
+        response = self.client.get(reverse('addNewEvent'))
+        self.assertRedirects(response, reverse('login') + f'?next={reverse("addNewEvent")}')
 
-    # def test_add_event_view_redirects_to_login_for_anonymous_user(self):
-    #     response = self.client.get(reverse('addNewEvent'))
+    def test_event_detail_view(self):
+        event = Event.objects.create(
+            event_title="Test Event",
+            event_latitude=40.7128,
+            event_longitude=-74.0060,
+            event_street_address="123 Main St",
+            event_city="New York",
+            event_state="NY",
+        )
+        response = self.client.get(reverse('event', args=[event.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Test Event")
 
-    #     # Check if the response redirects to the login page
-    #     self.assertRedirects(response, reverse('login') + f'?next={reverse("addNewEvent")}')
-
-    # def test_create_event(self):
-    #     user = User.objects.create_user(
-    #         username="testuser",
-    #         password="testpassword",
-    #     )
-    #     self.client.login(username="testuser", password="testpassword")
-
-    #     event_data = {
-    #         'event_title': 'Test Event',
-    #         'event_latitude': 40.7128,
-    #         'event_longitude': -74.0060,
-    #         'event_street_address': '123 Main St',
-    #         'event_city': 'New York',
-    #         'event_state': 'NY',
-    #     }
-
-    #     response = self.client.post(reverse('addNewEvent'), event_data)
-    #     self.assertRedirects(response, reverse('hoo_event:index'))
-    #     self.assertTrue(Event.objects.filter(event_title=event_data['event_title']).exists())
+    def test_recent_events_view(self):
+        response = self.client.get(reverse('recent_events'))
+        self.assertEqual(response.status_code, 200)
+        # Add more assertions based on what you expect to see in the recent events view.
