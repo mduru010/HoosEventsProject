78d06365d5f59d6f0b1ba890e4b899bbc867823f
nathan <myr4vt@virginia.edu>
2023-10-22 15:52:22 -0400

    Testing

--- hoo_event/urls.py
+++ hoo_event/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from . import views
 from django.views.generic import TemplateView
+from django.contrib import admin
+from django.urls import include, path
 
 app_name = "hoo_event"
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('event/<int:event_id>/', views.event, name='event'),
     path('', TemplateView.as_view(template_name="index.html"), name="index"),
     path('add/', views.addEvent, name="addNewEvent"),
     path('recent/', views.ShowRecentView.as_view(), name="recent"),
+    path('hoo_event/', include('hoo_event.urls')),
 ]
