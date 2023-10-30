e094ee767d807da8c12ee54244b642d0f9b8e5f4
Agi Luong <xwq5ja@virginia.edu>
2023-10-30 02:29:18 -0400

    added an enum to keep track of whether an event has been accepted yet or not

--- hoo_event/models.py
+++ hoo_event/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django import forms
 from django.utils import timezone
+from enum import Enum
 
 # Create your models here.
 from django.contrib.auth.models import Group, Permission
@@ -8,7 +9,10 @@ from django.contrib.auth.models import Group, Permission
 regular_users, created = Group.objects.get_or_create(name ='regular_users')
 admin_users, created = Group.objects.get_or_create(name ='admin_users')
 
-
+class EventStatus(Enum):
+    PENDING = 1
+    APPROVED = 2
+    DENIED = 3
 
 # Create your models here.
 class EventForm(forms.Form):
@@ -29,6 +33,7 @@ class Event(models.Model):
     event_state = models.CharField(max_length=100)
     event_start_time = models.DateTimeField(default="2000-01-01T00:00")
     event_end_time = models.DateTimeField(default="2000-01-01T00:00")
+    event_status = models.CharField(default=EventStatus.PENDING)
 
     def __str__(self):
         return self.event_title
