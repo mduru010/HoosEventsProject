31bf8f9db746ec99d9971621e77490e7749aeb87
nathan <myr4vt@virginia.edu>
2023-11-01 15:05:34 -0400

    Added tracking for approval status in Event

--- hoo_event/admin.py
+++ hoo_event/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
-
+from .models import Event
 # Register your models here.
 from django.contrib.auth.models import Group, Permission
 
 regular_users, created = Group.objects.get_or_create(name ='regular_users')
 admin_users, created = Group.objects.get_or_create(name ='admin_users')
+
+def approve_events(modeladmin, request, queryset):
+    # Mark selected events as approved
+    queryset.update(approved=True)
+
+approve_events.short_description = "Approve selected events"
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('event_title', 'approved', 'event_street_address', 'event_city', 'event_state')
+    actions = [approve_events]
+
+admin.site.register(Event, EventAdmin)
