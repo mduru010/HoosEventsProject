2e6f843db63cb00f17a59b9ebabe94992472bf80
nathan <myr4vt@virginia.edu>
2023-11-01 20:29:01 -0400

    Fixed the approved status test

--- hoo_event/tests.py
+++ hoo_event/tests.py
@@ -49,70 +49,6 @@ class EventModelTest(unittest.TestCase):
 
 ##################
 
-    # def test_event_creation(self):
-    #     # Test creating an event using the view
-    #     client = Client()
-    #     user = User.objects.create_user(
-    #         username=f"unique_testuser_{int(time.time())}",
-    #         password="testpassword",
-    #     )
-    #     client.login(username=user.username, password="testpassword")
-
-    #     # Define start and end times in a valid format (replace with actual date and time)
-    #     start_time = datetime(2023, 11, 1, 10, 0, 0)
-    #     end_time = datetime(2023, 11, 1, 12, 0, 0)
-
-    #     event_data = {
-    #         'event_title': 'test_event_creation',
-    #         'event_street_address': '456 Elm St',
-    #         'event_city': 'Los Angeles',
-    #         'event_state': 'CA',
-    #         'event_start_time': start_time,  # Use valid date and time
-    #         'event_end_time': end_time,  # Use valid date and time
-    #     }
-
-    #     response = client.post(reverse('hoo_event:addNewEvent'), event_data)
-    #     self.assertEqual(response.status_code, 200)  # Check for successful redirect after form submission
-
-    #     # Get all events with the title 'test_event_creation'
-    #     events = Event.objects.filter(event_title='test_event_creation')
-    #     print(events)
-    #     # Check if at least one event with the given title exists
-    #     self.assertTrue(events.exists())
-
-    #     # You can also check other attributes of the events if needed
-    #     for new_event in events:
-    #         self.assertEqual(new_event.event_title, event_data['event_title'])
-    #         self.assertEqual(new_event.event_street_address, event_data['event_street_address'])
-    #         self.assertEqual(new_event.event_city, event_data['event_city'])
-    #         self.assertEqual(new_event.event_state, event_data['event_state'])
-    #         self.assertEqual(new_event.event_status, EventStatus.PENDING)  # Check that the event is approved
-
-    # def test_event_creation(self):
-    #     # Test creating an event using the view
-    #     client = Client()
-    #     user = User.objects.create_user(
-    #         username=f"unique_testuser_{int(time.time())}",
-    #         password="testpassword",
-    #     )
-    #     client.login(username=user.username, password="testpassword")
-
-    #     # Define start and end times in a valid format (replace with actual date and time)
-    #     start_time = datetime(2023, 11, 1, 10, 0, 0)
-    #     end_time = datetime(2023, 11, 1, 12, 0, 0)
-
-    #     event_data = {
-    #         'event_title': 'test_event_creation',
-    #         'event_street_address': '456 Elm St',
-    #         'event_city': 'Los Angeles',
-    #         'event_state': 'CA',
-    #         'event_start_time': start_time,  # Use valid date and time
-    #         'event_end_time': end_time,  # Use valid date and time
-    #     }
-
-    #     response = client.post(reverse('hoo_event:addNewEvent'), event_data)
-    #     self.assertEqual(response.status_code, 302)  # Check for a redirect (status code 302)
-
     def test_event_creation(self):
         # Test creating an event using the view
         client = Client()
@@ -168,18 +104,19 @@ class EventModelTest(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTrue("Test Event" in response.content.decode())
 
-    # def test_recent_events_view(self):
-    #     # Create a test event with the approved field set to True
-    #     event = Event.objects.create(
-    #         event_title="Test Event",
-    #         event_latitude=40.7128,
-    #         event_longitude=-74.0060,
-    #         event_street_address="123 Main St",
-    #         event_city="New York",
-    #         event_state="NY",
-    #         approved=True,  # Set the approved field to True
-    #     )
-    #     client = Client()
-    #     response = client.get(reverse('hoo_event:recent'))
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTrue("Test Event" in response.content.decode())
+    def test_recent_events_view(self):
+        # Create a test event with the event status set to approved
+        event = Event.objects.create(
+            event_title="Test Event",
+            event_latitude=40.7128,
+            event_longitude=-74.0060,
+            event_street_address="123 Main St",
+            event_city="New York",
+            event_state="NY",
+            event_status=EventStatus.APPROVED,  # Set the event status to approved
+        )
+
+        client = Client()
+        response = client.get(reverse('hoo_event:recent'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("Test Event" in response.content.decode())
