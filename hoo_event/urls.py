1d0b997aa289ac1cd3073e9ed74e6dc5ed062ae7
Daniel Huynh <danielhuynh523@gmail.com>
2023-11-15 21:26:38 -0500

    added a home page with a map, populated with markers of each approved event

--- hoo_event/urls.py
+++ hoo_event/urls.py
@@ -8,6 +8,7 @@ app_name = "hoo_event"
 urlpatterns = [
     path('event/<int:event_id>/', views.event, name='event'),
     path('', TemplateView.as_view(template_name="index.html"), name="index"),
+    path('home/', views.home, name="home"),
     path('add/', views.addEvent, name="addNewEvent"),
     path('recent/', views.ShowRecentView.as_view(), name="recent"),
     path('pending/', views.ShowPendingView.as_view(), name="pending"),
